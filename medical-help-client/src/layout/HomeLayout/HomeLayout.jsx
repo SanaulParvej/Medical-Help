269fe863ce44f0fa3e79bbaa6369108fd2df25f9
Rabbyxq <www.rabbi0110110@gmail.com>
2026-02-26 00:13:36 +0600

    Added footer design and text

--- medical-help-client/src/layout/HomeLayout/HomeLayout.jsx
+++ medical-help-client/src/layout/HomeLayout/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../../Component/Shared/Navbar/Navbar';
+import Footer from '../../Component/Shared/Footer/footer';
 
 const HomeLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
