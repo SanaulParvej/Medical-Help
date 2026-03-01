8c697b2eb673c15d00247965dbd4745169bb8193
Rabbyxq <www.rabbi0110110@gmail.com>
2026-03-01 19:04:34 +0600

    add navbar function for logged user

--- medical-help-client/src/pages/Home/CTAservice.jsx
+++ medical-help-client/src/pages/Home/CTAservice.jsx
@@ -7,13 +7,13 @@ export default function CTAService() {
 
     return (
         <section className="py-14 bg-base-200 font-bangla">
-            <div className="max-w-6xl mx-auto px-6 lg:px-10">
-                <div className="hero bg-base-100 rounded-2xl shadow-xl border border-base-200">
+            <div className="">
+                <div className="hero bg-red-500 shadow-sm ">
                     <div className="hero-content text-center py-12 px-6">
-                        <div className="space-y-4">
-                            <h2 className="text-3xl lg:text-4xl font-bold text-gray-800">জরুরি সহায়তা প্রয়োজন?</h2>
-                            <p className="text-gray-500 max-w-2xl mx-auto">জরুরি চিকিৎসা সহায়তার জন্য এখনই ৯৯৯ নম্বরে কল করুন</p>
-                            <button onClick={handleEmergencyCall} className="btn btn-error text-white btn-lg mt-2">
+                        <div className="space-y-4 ">
+                            <h2 className="text-3xl lg:text-4xl font-bold text-base-100">জরুরি সহায়তা প্রয়োজন?</h2>
+                            <p className="text-base-100 max-w-2xl mx-auto">জরুরি চিকিৎসা সহায়তার জন্য এখনই ৯৯৯ নম্বরে কল করুন</p>
+                            <button onClick={handleEmergencyCall} className="btn btn-error text-white btn-lg mt-2 px-8 btn-wide max-w-xs w-full sm:w-auto">
                                 এখনই ৯৯৯ এ কল করুন
                             </button>
                         </div>
