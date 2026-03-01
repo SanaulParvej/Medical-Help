3bb18e5fb8ffd854a7642ed139717cac0c85f4c0
apupaul1 <apupaul.dev@gmail.com>
2026-03-01 11:56:35 +0600

    Created AuthContext and AuthProvider for state  management and Private Routes for protected Route

---
+++ medical-help-client/src/contexts/AuthContext/AuthProvider.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { auth } from '../../firebase/firebase.init'
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signOut, signInWithPopup, onAuthStateChanged, updateProfile } from 'firebase/auth';
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState(null)
+
+    const userSignUp = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const userLogin = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const userSignOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const updateUser = (displayName) => {
+        return updateProfile(auth.currentUser, {
+            displayName: displayName
+        })
+    }
+
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        loading,
+        user,
+        userSignUp,
+        googleSignIn,
+        userLogin,
+        userSignOut,
+        updateUser
+    }
+
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
