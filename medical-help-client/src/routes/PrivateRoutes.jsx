2dc6612c96a083774a48f28905e76d331ec30f1e
apupaul1 <apupaul.dev@gmail.com>
2026-03-08 22:12:15 +0600

    Added Dashboard UI and functionalities of sidebar

--- medical-help-client/src/routes/PrivateRoutes.jsx
+++ medical-help-client/src/routes/PrivateRoutes.jsx
@@ -3,18 +3,18 @@ import { use } from 'react';
 import { AuthContext } from '../contexts/AuthContext/AuthContext';
 import { Navigate, useLocation } from 'react-router';
 
-const PrivateRoutes = ({children}) => {
+const PrivateRoutes = ({ children }) => {
 
-const {user,loading} = use(AuthContext);
-const location = useLocation()
+    const { user, loading } = use(AuthContext);
+    const location = useLocation()
 
-if(loading){
-    return <span className='loading loading-bars loading-xl'></span>
-}
+    if (loading) {
+        return <span className='loading loading-bars loading-xl'></span>
+    }
 
-if(!user) {
-   return <Navigate to={'/signin'} state={location.pathname}></Navigate>
-}
+    if (!user) {
+        return <Navigate to={'/auth/login'} state={location.pathname}></Navigate>
+    }
 
     return children;
 };
