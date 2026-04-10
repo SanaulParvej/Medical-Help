a58a557e5b6239211c4d64f375f15833241aa74e
Perves <perves@example.com>
2026-04-10 21:45:18 +0600

    Added PhysiotherapyBookings

--- medical-help-client/src/pages/User/MyBookings/MyBookings.jsx
+++ medical-help-client/src/pages/User/MyBookings/MyBookings.jsx
@@ -1,37 +1,38 @@
-import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import MyNursingCareBookings from './MyNursingCareBookings';
-import MyHomeCareBookings from './MyHomeCareBookings';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import MyNursingCareBookings from "./MyNursingCareBookings";
+import MyHomeCareBookings from "./MyHomeCareBookings";
+import MyPhysiotherapyBookings from "./MyPhysiotherapyBookings";
 
 const MyBookings = () => {
-    return (
-        <div className='p-6'>
-            <div className='mb-8'>
-                <h1 className='text-3xl'>Your Bookings Here</h1>
-                <p>Explore all bookings </p>
-            </div>
-            <div>
-                <Tabs>
-                    <TabList>
-                        <Tab>Nursing Care</Tab>
-                        <Tab>Home Care</Tab>
-                        <Tab>Physiotherapy</Tab>
-                    </TabList>
+  return (
+    <div className="p-6">
+      <div className="mb-8">
+        <h1 className="text-3xl">Your Bookings Here</h1>
+        <p>Explore all bookings </p>
+      </div>
+      <div>
+        <Tabs>
+          <TabList>
+            <Tab>Nursing Care</Tab>
+            <Tab>Home Care</Tab>
+            <Tab>Physiotherapy</Tab>
+          </TabList>
 
-                    <TabPanel>
-                        <MyNursingCareBookings></MyNursingCareBookings>
-                    </TabPanel>
-                    <TabPanel>
-                        <MyHomeCareBookings/>
-                    </TabPanel>
-                    <TabPanel>
-                        <h2>Alll</h2>
-                    </TabPanel>
-                </Tabs>
-            </div>
-        </div>
-    );
+          <TabPanel>
+            <MyNursingCareBookings></MyNursingCareBookings>
+          </TabPanel>
+          <TabPanel>
+            <MyHomeCareBookings />
+          </TabPanel>
+          <TabPanel>
+            <MyPhysiotherapyBookings></MyPhysiotherapyBookings>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default MyBookings;
